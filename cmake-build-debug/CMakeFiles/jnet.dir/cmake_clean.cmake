a34b6e3b2cd9bc8bad5f6a248f4b60ef1528590e
Jacob Baird <jacob135893@gmail.com>
2018-12-19 21:55:04 -0400

    jacob/12/19/2018: Initialize CLion project

---
+++ cmake-build-debug/CMakeFiles/jnet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jnet.dir/main.c.obj"
+  "jnet.pdb"
+  "jnet.exe"
+  "jnet.exe.manifest"
+  "libjnet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jnet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
